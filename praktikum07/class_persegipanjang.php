48184c9aa950ca9876dbd2cc7ebccf64aff099d1
tedy_rr <65979721+TedyRR@users.noreply.github.com>
2023-07-12 00:55:51 +0700

    Add files via upload

---
+++ praktikum07/class_persegipanjang.php
@@ -0,0 +1,17 @@
+<?php 
+    class persegipanjang{
+        public $panjang, $lebar;
+        public function __construct($panjang, $lebar){
+            $this->panjang = $panjang;
+            $this->lebar = $lebar;
+        }
+        public function luas(){
+            $luas = $this->panjang * $this->lebar;
+            return $luas;
+        }
+        public function keliling(){
+            $keliling = (2*$this->panjang)+(2*$this->lebar);
+            return $keliling;
+        }
+    } 
+?>
